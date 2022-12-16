bb657a3e9cb89511f595db0e6a9734d11f7a3224
Matthias Kreier <matthias@kreier.org>
2022-12-16 22:25:38 +0700

    Initial Commit

---
+++ AppDev 21/AppDev_21App.swift
@@ -0,0 +1,17 @@
+//
+//  AppDev_21App.swift
+//  AppDev 21
+//
+//  Created by Matthias Kreier on 16/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct AppDev_21App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
